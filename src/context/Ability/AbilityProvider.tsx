97a754df1132d26ab33a072cc664b41e305c2047
hungpurdie <nmhung.dev@gmail.com>
2022-07-29 15:14:41 +0700

    (feat): use casl for authoriztion

---
+++ src/context/Ability/AbilityProvider.tsx
@@ -0,0 +1,20 @@
+import { createContext, useEffect, useState } from 'react';
+import { AppAbility, buildAbilityFor } from '~/configs/ability';
+import { useAuth } from '~/hooks/useAuth';
+
+export const AbilityContext = createContext<AppAbility>(undefined!);
+AbilityContext.displayName = 'AbilityContext';
+
+interface AbilityProviderProps {
+  children: React.ReactNode;
+}
+export default function AbilityProvider({ children }: AbilityProviderProps) {
+  const { user } = useAuth();
+  const [ability, setAbility] = useState(buildAbilityFor(user?.role || 'Guest'));
+
+  useEffect(() => {
+    setAbility(buildAbilityFor(user?.role || 'Guest'));
+  }, [user?.role]);
+
+  return <AbilityContext.Provider value={ability}>{children}</AbilityContext.Provider>;
+}
